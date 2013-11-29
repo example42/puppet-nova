5354df86bfaa38d73def559bcfc117eac9ca6af8
Alessandro Franceschi <al@lab42.it>
2013-11-29 10:25:53 +0100

    Renamed fucntion choose_default to default_content

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -70,7 +70,7 @@ define nova::conf (
   include nova
 
   $manage_path    = pickx($path, "${nova::config_dir_path}/${name}")
-  $manage_content = choose_default($content, $template)
+  $manage_content = default_content($content, $template)
   $manage_mode    = pickx($mode, $nova::config_file_mode)
   $manage_owner   = pickx($owner, $nova::config_file_owner)
   $manage_group   = pickx($group, $nova::config_file_group)
